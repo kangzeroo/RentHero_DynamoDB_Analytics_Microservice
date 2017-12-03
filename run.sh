db76515b511809e6c9dedec75dea0b1a8e9488fb
Kangze Huang <kangzeroo@gmail.com>
2017-12-03 10:09:43 -0500

    dockername

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -it -p 3007:3007 --name=DynamoDB_Analytics DynamoDB_Analytics npm run prod -- --host=0.0.0.0
+docker run -d -it -p 3007:3007 --name=dynamodb_analytics dynamodb_analytics npm run prod -- --host=0.0.0.0
