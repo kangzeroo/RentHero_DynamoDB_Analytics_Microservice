c154c74e2e8030d2cec61dfcb8793491c9ac318f
Kangze Huang <kangzeroo@gmail.com>
2017-12-13 18:31:50 -0500

    beggining of storylines

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -8,6 +8,8 @@ exports.get_all_users = function(req, res, next){
 
 // POST /get_specific_user
 exports.get_specific_user = function(req, res, next){
+  // this should be able to take a user's id, first name, phone or email and return a list of matching
+  // req.body = { user_id, user_first_name, user_last_name, user_email, user_phone }
   res.json({
     message: "Test says alive and well"
   })
