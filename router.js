a5316e35f287daf210a925ee74582fa6564bf918
Kangze Huang <kangzeroo@gmail.com>
2017-12-13 22:16:14 -0500

    added checks

--- router.js
+++ router.js
@@ -9,6 +9,7 @@ const UserQueries = require('./routes/Postgres_Leasing/Queries/UserQueries')
 // bodyParser attempts to parse any request into JSON format
 const json_encoding = bodyParser.json({type:'*/*'})
 const originCheck = require('./auth/originCheck').originCheck
+const JWT_Check = require('./auth/JWT_Check').JWT_Check
 // bodyParser attempts to parse any request into GraphQL format
 // const graphql_encoding = bodyParser.text({ type: 'application/graphql' })
 
@@ -26,17 +27,17 @@ module.exports = function(app){
 	app.post('/scan_dynamodb', [json_encoding], Analytics.scan_dynamodb)
 
 	// landlord website storyline
-	app.post('/get_user_building_browsing_history', [json_encoding], Storyline.get_user_building_browsing_history)
-	app.post('/get_user_suite_browsing_history', [json_encoding], Storyline.get_user_suite_browsing_history)
-	app.post('/get_user_preferences_history', [json_encoding], Storyline.get_user_preferences_history)
-	app.post('/get_user_images_history', [json_encoding], Storyline.get_user_images_history)
+	app.post('/get_user_building_browsing_history', [json_encoding, JWT_Check, originCheck], Storyline.get_user_building_browsing_history)
+	app.post('/get_user_suite_browsing_history', [json_encoding, JWT_Check, originCheck], Storyline.get_user_suite_browsing_history)
+	app.post('/get_user_preferences_history', [json_encoding, JWT_Check, originCheck], Storyline.get_user_preferences_history)
+	app.post('/get_user_images_history', [json_encoding, JWT_Check, originCheck], Storyline.get_user_images_history)
 
 	// User queries
-	app.get('/get_all_users', [json_encoding], UserQueries.get_all_users)
-	app.post('/get_specific_user_by_id', [json_encoding], UserQueries.get_specific_user_by_id)
-	app.post('/get_specific_user_by_email', [json_encoding], UserQueries.get_specific_user_by_email)
-	app.post('/get_specific_user_by_phone', [json_encoding], UserQueries.get_specific_user_by_phone)
-	app.post('/get_specific_user_by_first_name', [json_encoding], UserQueries.get_specific_user_by_first_name)
-	app.post('/get_specific_user_by_last_name', [json_encoding], UserQueries.get_specific_user_by_last_name)
-	app.post('/get_specific_user_by_full_name', [json_encoding], UserQueries.get_specific_user_by_full_name)
+	app.get('/get_all_users', [json_encoding, JWT_Check, originCheck], UserQueries.get_all_users)
+	app.post('/get_specific_user_by_id', [json_encoding, JWT_Check, originCheck], UserQueries.get_specific_user_by_id)
+	app.post('/get_specific_user_by_email', [json_encoding, JWT_Check, originCheck], UserQueries.get_specific_user_by_email)
+	app.post('/get_specific_user_by_phone', [json_encoding, JWT_Check, originCheck], UserQueries.get_specific_user_by_phone)
+	app.post('/get_specific_user_by_first_name', [json_encoding, JWT_Check, originCheck], UserQueries.get_specific_user_by_first_name)
+	app.post('/get_specific_user_by_last_name', [json_encoding, JWT_Check, originCheck], UserQueries.get_specific_user_by_last_name)
+	app.post('/get_specific_user_by_full_name', [json_encoding, JWT_Check, originCheck], UserQueries.get_specific_user_by_full_name)
 }
