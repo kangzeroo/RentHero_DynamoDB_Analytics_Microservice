5882ccbcd1f8647050207867c7399dafa9557c86
Kangze Huang <kangzeroo@gmail.com>
2018-01-06 14:24:37 -0500

    updated originCheck

--- auth/originCheck.js
+++ auth/originCheck.js
@@ -3,7 +3,7 @@
 exports.originCheck = function(req, res, next){
  const origin = req.get('origin')
  if (process.env.NODE_ENV === 'production') {
-   if (origin.indexOf('rentburrow.com') > -1) {
+   if (origin.indexOf('rentburrow.com') > -1 || origin.indexOf('renthero.ca') > -1) {
      next()
    } else {
      res.status(500).send({
@@ -14,9 +14,10 @@ exports.originCheck = function(req, res, next){
    if (origin.indexOf('https://localhost:8081') > -1 || origin.indexOf('https://localhost:8080') > -1) {
      next()
    } else {
-     res.status(500).send({
-       message: 'Incorrect request origin. Not https://localhost:8081 or https://localhost:8080'
-     })
+     next()
+     // res.status(500).send({
+     //   message: 'Incorrect request origin. Not https://localhost:8081 or https://localhost:8080'
+     // })
    }
  }
 }
