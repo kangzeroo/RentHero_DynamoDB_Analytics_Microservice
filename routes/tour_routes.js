1b3c1d5337249dc57d213f1224e6169bad4ffbcb
Kangze Huang <kangzeroo@gmail.com>
2018-01-14 00:40:37 -0500

    mhmm

--- routes/tour_routes.js
+++ routes/tour_routes.js
@@ -4,18 +4,17 @@ const verify_relevant_hints = require('../DynamoDB/dynamodb_chat_thread_api').ve
 
 // POST /get_chat_thread
 exports.get_chat_thread = function(req, res, next){
-  console.log('hello')
   const sender_id = req.body.sender_id
   const receiver_id = req.body.receiver_id
 
   get_specific_chat_thread(sender_id, receiver_id)
-  .then((data) => {
-    console.log(data)
-    res.json(data)
-  }).catch((err) => {
-    console.log(err)
-    res.status(500).send(err)
-  })
+    .then((data) => {
+      // console.log(data)
+      res.json(data)
+    }).catch((err) => {
+      console.log(err)
+      res.status(500).send(err)
+    })
 }
 
 // POST /clear_relevant_hints
